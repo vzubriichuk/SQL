0205ab8e6a95889e098c9f964cb96f29e979d1c7
v.zubriichuk <v@zubr.kiev.ua>
2021-03-11 13:20:27 +0200

    Example SQL with TIES

---
+++ TIES_example.sql
@@ -0,0 +1,21 @@
+	
+	-- Example TIES
+	declare @goals table (id int identity, id_player int, time_ varchar(10))
+	
+	insert  @goals(id_player, time_) values(6,'45:12')
+	insert  @goals(id_player, time_) values(6,'72:00')
+	insert  @goals(id_player, time_) values(3,'74:42')
+	insert  @goals(id_player, time_) values(2,'82:10')
+	insert  @goals(id_player, time_) values(3,'84:19')
+
+	-- without TIES
+	select * 
+	from @goals
+
+	-- with TIES
+	select top 1 with TIES 
+			id_player
+		,	count(*)
+	from @goals
+	group by id_Player
+	order by count(*) desc
